0eec8fd9e329c9852fb32ec43c460d5f3781e78c
momo <herr_moise@msn.com>
2018-12-05 22:13:19 +0000

    we know what we would touch

--- src/components/barChartContainer.tsx
+++ src/components/barChartContainer.tsx
@@ -4,23 +4,24 @@ import { BarChart, Bar, XAxis, YAxis, Cell, CartesianGrid, Tooltip, Legend } fro
 /**
  * The key thing to observe about rechart is that the lineChart is like the container of 
  */
+
+ // {
+    //     "AnswerRef": "one",
+    //     "Text": "5 out of 50 throws",
+    //     "foo": -12,
+    //     "Score": 0,
+    //     "RespondentPercentage": 12,
+    //     "Rank": 1
+    // },
+    // {
+    //     "AnswerRef": "one",
+    //     "Text": "5 out of 50 throws",
+    //     "Score": 0,
+    //     "foo": 0,
+    //     "RespondentPercentage": 0,
+    //     "Rank": 1
+    // },
 const data = [
-    {
-        "AnswerRef": "one",
-        "Text": "5 out of 50 throws",
-        "foo": -12,
-        "Score": 0,
-        "RespondentPercentage": 12,
-        "Rank": 1
-    },
-    {
-        "AnswerRef": "one",
-        "Text": "5 out of 50 throws",
-        "Score": 0,
-        "foo": 0,
-        "RespondentPercentage": 0,
-        "Rank": 1
-    },
     {
         "AnswerRef": "one",
         "Text": "5 out of 50 throws",
@@ -45,6 +46,33 @@ class LineChartContainer extends React.Component {
                     margin={{ top: 5, right: 0, left: 0, bottom: 25 }}>
                     <XAxis
                         dataKey="foo"
+                        type="number"
+                        domain = {[-18, 18]}//TODO add datamax and datamin to the data you had
+                        ticks ={[0]}
+                    />
+                    {/* <YAxis hide /> */}
+                    <Bar
+                        dataKey="RespondentPercentage"
+                        barSize={10}
+                        fontFamily="sans-serif"
+                    >
+
+                    </Bar>
+                </BarChart>
+            </div>
+        )
+    }
+}
+
+/**
+ *                 <BarChart
+                    stackOffset="sign"
+                    width={700}
+                    height={260}
+                    data={data}
+                    margin={{ top: 5, right: 0, left: 0, bottom: 25 }}>
+                    <XAxis
+                        dataKey="foo"
                     />
                     <YAxis hide />
                     <CartesianGrid
@@ -60,9 +88,6 @@ class LineChartContainer extends React.Component {
 
                     </Bar>
                 </BarChart>
-            </div>
-        )
-    }
-}
+ */
 
 export default LineChartContainer
