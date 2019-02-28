39b8e6398f37f044b055f5e6014772160d52b075
Moise Lubwimi <moise.lubwimi@weareadaptive.com>
2019-02-28 18:01:19 +0000

    cannot figure out what the items is

--- src/animation/Flop.tsx
+++ src/animation/Flop.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
+import { Transition } from 'react-spring/renderprops'
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
@@ -21,6 +21,19 @@ const DivTwo = styled.div`
     background-color: red;
 `
 
+const Foo: React.FC = () => {
+    return <Transition
+        items={[{ key: 'stuff', toggle: true }]}
+        from={{ position: 'absolute', opacity: 0 }}
+        enter={{ opacity: 1 }}
+        leave={{ opacity: 0 }}>
+        {toggle =>
+            toggle
+                ? props => <div style={props}>Show Me Nothing</div>
+                : props => <div style={props}>Show Me</div>
+        }
+    </Transition>
+}
 interface Move {
     shouldMove: boolean
 }
@@ -65,7 +78,7 @@ const Container: React.FC = () => {
             <div>
                 <button onClick={() => setIsCardOne(!isCardOne)}>Toggle</button>
             </div>
-            <Component />
+            <Foo />
         </Wrapper>
     )
 }
